61e3e4cc00f047ef14b06c9dc151bb9b197a7743
Cherry Liang <cherryliang@Cherrys-MacBook-Pro.local>
2021-12-14 19:06:27 +1300

    fix: table insert  error

--- src/mappings/Staking.ts
+++ src/mappings/Staking.ts
@@ -7,16 +7,20 @@ export async function handleStakingRewarded(event: SubstrateEvent): Promise<void
 }
 
 export async function handleStakingReward(event: SubstrateEvent): Promise<void> {
-  const {
-    event: {
-      data: [account, newReward],
-    },
-  } = event;
-  const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
-  entity.accountId = account.toString();
-  entity.balance = (newReward as Balance).toBigInt();
-  entity.date = event.block.timestamp;
-  await entity.save();
+  try {
+    const {
+      event: {
+        data: [account, newReward],
+      },
+    } = event;
+    const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
+    entity.accountId = account.toString();
+    entity.balance = (newReward as Balance).toBigInt();
+    entity.date = event.block.timestamp;
+    await entity.save();
+  } catch (error) {
+    logger.error(error);
+  }
 }
 
 export async function handleStakingSlashed(event: SubstrateEvent): Promise<void> {
@@ -24,14 +28,18 @@ export async function handleStakingSlashed(event: SubstrateEvent): Promise<void>
 }
 
 export async function handleStakingSlash(event: SubstrateEvent): Promise<void> {
-  const {
-    event: {
-      data: [account, newSlash],
-    },
-  } = event;
-  const entity = new StakingSlash(`${event.block.block.header.number}-${event.idx.toString()}`);
-  entity.accountId = account.toString();
-  entity.balance = (newSlash as Balance).toBigInt();
-  entity.date = event.block.timestamp;
-  await entity.save();
+  try {
+    const {
+      event: {
+        data: [account, newSlash],
+      },
+    } = event;
+    const entity = new StakingSlash(`${event.block.block.header.number}-${event.idx.toString()}`);
+    entity.accountId = account.toString();
+    entity.balance = (newSlash as Balance).toBigInt();
+    entity.date = event.block.timestamp;
+    await entity.save();
+  } catch (error) {
+    logger.error(error);
+  }
 }
