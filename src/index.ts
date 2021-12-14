457638e80cbbc5344910a773b10fe59b03f991a7
Cherry Liang <cherryliang@Cherrys-MacBook-Pro.local>
2021-12-14 18:45:57 +1300

    fix: mappingFunc error

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './mappings/Handlers';
 export * from './mappings/NewSessionHandler';
 export * from './mappings/ValidatorsHandler';
+export * from './mappings/EraPaidHandler';
+export * from './mappings/ValidatorPayoutHandler';
+export * from './mappings/Staking';
+export * from './mappings/RewardAndSlash';
